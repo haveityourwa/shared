ccc6b17bd6aa6b19a9eb8159db7007339b6d21de
haveityourwa <anthony.elkins@my.jcu.edu.au>
2019-06-27 13:34:12 +1000

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,15 @@ int* test1 = 1;
 int* test2 = 2;
 
 int main(){
-    printf("HelloWorld1");
+    //printf("HelloWorld1 \n");
     test();
-    printf("%i",test2);
+    printf("number before %i \n",test2);
     test_csub_(&test1,&test2);
-    printf("%i",test2);
+    printf("number after %i \n",test2);
 };
 
 void test(){
-    printf("helloWorld");
+    //printf("helloWorld \n");
 };
 
 void test_csub_(int* arg, int* res){
